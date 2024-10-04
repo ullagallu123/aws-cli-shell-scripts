55798590b770c79e36d992da508ac2482cf830d4
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-04 21:44:03 +0530

    removing comments

--- ta-install.sh
+++ ta-install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
-# Check if Terraform installed successfully
+
 if terraform --version &> /dev/null; then
   echo "Terraform installed successfully."
 else
@@ -13,7 +12,6 @@ else
   exit 1
 fi
 
-# Add Terraform aliases
 echo "alias t='terraform'" >> ~/.bash_profile
 echo "alias ti='terraform init'" >> ~/.bash_profile
 echo "alias tf='terraform fmt'" >> ~/.bash_profile
@@ -22,10 +20,9 @@ echo "alias tp='terraform plan'" >> ~/.bash_profile
 echo "alias ta='terraform apply -auto-approve'" >> ~/.bash_profile
 echo "alias td='terraform destroy -auto-approve'" >> ~/.bash_profile
 
-# Source the bash profile to apply aliases
+echo "Reloading the ~/.bash_profile"
 source ~/.bash_profile
 
-# Install Python and pip if not installed
 if ! command -v python3 &> /dev/null; then
   sudo yum -y install python3
 fi
@@ -34,10 +31,8 @@ if ! command -v pip3 &> /dev/null; then
   sudo yum -y install python3-pip
 fi
 
-# Install Ansible
 pip3 install ansible
 
-# Check if Ansible installed successfully
 if ansible --version &> /dev/null; then
   echo "Ansible installed successfully."
 else
